8188f5ad18ecf85ddf74348c1bf9871c1dcd1462
al-abbas-nz <al.mus930@gmail.com>
2021-01-30 21:40:18 +1300

    rendering data from sheets into MusicStock component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MusicStock from './components/MusicStock'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <MusicStock />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MusicStock.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Tabletop from "tabletop";
+
+export default function MusicStock() {
+    const [data, setData] = useState({}); 
+    useEffect(() => {
+        Tabletop.init({
+            key: process.env.REACT_APP_GOOGLE_DOC_ID,
+            simpleSheet: true,
+        }).then(function (data) {
+            setData(data);
+        });
+    }, []);
+    const music = Array.from(data); 
+    console.log(music)
+    return (
+        <>
+            <ul>
+                {music.map((record) => (
+                    <div key={record.catalogID}>
+                        <h1>Arist: {record.artist ? record.artist : record.title}</h1>
+                        <h2>Title: {record.title}</h2>
+                        <p>{`Stock Level: ${record.stockLevelAR} | Central Warehouse: ${record.stockLevelCW}`}</p>
+                        <ul>
+                            <li>Release Date: {record.releaseDate}</li>
+                            <li>Label: {record.label}</li>
+                            <li>Format: {record.itemFormat}</li>
+                        </ul>
+                    </div>
+                ))}
+            </ul>
+        </>
+        )
+};
