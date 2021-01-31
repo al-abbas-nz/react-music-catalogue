29c0cdbe03f381f08d4a7e9c8c86c67219e8001c
al-abbas-nz <al.mus930@gmail.com>
2021-01-31 14:22:09 +1300

    add basic styling

--- src/components/MusicStock.js
+++ src/components/MusicStock.js
@@ -14,21 +14,19 @@ export default function MusicStock() {
     const music = Array.from(data); 
     console.log(music)
     return (
-        <>
-            <ul>
-                {music.map((record) => (
-                    <div key={record.catalogID}>
-                        <h1>Arist: {record.artist ? record.artist : record.title}</h1>
-                        <h2>Title: {record.title}</h2>
-                        <p>{`Stock Level: ${record.stockLevelAR} | Central Warehouse: ${record.stockLevelCW}`}</p>
-                        <ul>
-                            <li>Release Date: {record.releaseDate}</li>
-                            <li>Label: {record.label}</li>
-                            <li>Format: {record.itemFormat}</li>
-                        </ul>
-                    </div>
-                ))}
-            </ul>
-        </>
+        <div className='card-container'>
+            {music.map((record) => (
+                <div className='record-card' key={record.catalogID}>
+                    <h1>Artist: {record.artist ? record.artist : record.title}</h1>
+                    <h2>Title: {record.title}</h2>
+                    <p>{`Stock Level: ${record.stockLevelAR} | Central Warehouse: ${record.stockLevelCW}`}</p>
+                    <ul>
+                        <li>Release Date: {record.releaseDate}</li>
+                        <li>Label: {record.label}</li>
+                        <li>Format: {record.itemFormat}</li>
+                    </ul>
+                </div>
+            ))}
+            </div>
         )
 };
